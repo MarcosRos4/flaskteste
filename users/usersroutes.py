1b80d7d83f990e9a35c7a790cb321231b2c7e275
Kriguer <77925501+zKriguer@users.noreply.github.com>
2023-04-05 19:09:44 -0300

    Update usersroutes.py

--- users/usersroutes.py
+++ users/usersroutes.py
@@ -10,8 +10,9 @@ users_bp = Blueprint(
 # retornar todos os usuarios da tabela usuarios: nome, telefone, email, templates
 @users_bp.route('/users/get-all',methods=['GET'])
 def get_user_all():
+    print(supabase)
     user = supabase.table('users').select("*").execute().data
-    
+    print(user)
     return jsonify(user)
 
 # retornar informações da tabela usuarios: nome, telefone, email, templates
