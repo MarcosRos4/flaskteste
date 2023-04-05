6867e62d7445b60b7eda97a8937fc003a4461d21
Kriguer <77925501+zKriguer@users.noreply.github.com>
2023-04-05 18:51:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,5 @@ with app.app_context():
     app.register_blueprint(templates_bp)
     app.register_blueprint(payments_bp)
         
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
